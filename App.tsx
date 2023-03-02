6fed976a10481391b0d6682bc3716d04a714a300
Matej Pavlík <Snip3rSVK@gmail.com>
2023-03-02 17:03:38 +0100

    Initial setup

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Button title="HELLO" />
+    </View>
+  );
+}
+
+export default App;
