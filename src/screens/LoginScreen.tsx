203a0cd6d7dd2aec596636b4cf7e2a2bb1ff492e
Matej Pavlík <Snip3rSVK@gmail.com>
2023-04-04 12:33:13 +0200

    Add LoginScreen, RegisterScreen, Add use-immer

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,14 +1,68 @@
-import {Button, View} from 'native-base';
-import React from 'react';
+import {Box, Button, FormControl, Input, Text, View, VStack} from 'native-base';
+import React, {useState} from 'react';
 import {RootStackScreenProps} from '../types/navigation';
 
 export default function LoginScreen({
   navigation,
 }: RootStackScreenProps<'Login'>) {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  function onSubmit() {
+    // TODO
+    // console.log('FORM DATA', formData);
+  }
+
   return (
     <View>
-      <Button>Login</Button>
-      <Button onPress={() => navigation.navigate('Register')}>Register</Button>
+      <Box pt="6" pb="5" alignItems="center">
+        <Text fontSize="5xl" fontWeight="bold">
+          COM.LY
+        </Text>
+      </Box>
+
+      <VStack px="2" space="9">
+        <VStack space="5">
+          <FormControl>
+            <FormControl.Label>Username</FormControl.Label>
+            <Input
+              onChangeText={value =>
+                setFormData({...formData, username: value})
+              }
+              type="text"
+              variant="filled"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input
+              onChangeText={value =>
+                setFormData({...formData, password: value})
+              }
+              type="password"
+              variant="filled"
+            />
+          </FormControl>
+        </VStack>
+
+        <VStack alignItems="center">
+          <Button onPress={onSubmit} width="full">
+            Login
+          </Button>
+
+          <Text mt="6" mb="2">
+            Don't have an account?
+          </Text>
+          <Button
+            onPress={() => navigation.navigate('Register')}
+            variant="outline">
+            Register
+          </Button>
+        </VStack>
+      </VStack>
     </View>
   );
 }
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,4 +1,4 @@
-import {Button, View} from 'native-base';
+import {Box, Button, FormControl, Input, Text, View, VStack} from 'native-base';
 import React from 'react';
 import {RootStackScreenProps} from '../types/navigation';
 
@@ -7,8 +7,43 @@ export default function RegisterScreen({
 }: RootStackScreenProps<'Register'>) {
   return (
     <View>
-      <Button>Register</Button>
-      <Button onPress={() => navigation.navigate('Login')}>Login</Button>
+      <Box pt="6" pb="5" alignItems="center">
+        <Text fontSize="5xl" fontWeight="bold">
+          COM.LY
+        </Text>
+      </Box>
+
+      <VStack px="2" space="9">
+        <VStack space="5">
+          <FormControl>
+            <FormControl.Label>Email</FormControl.Label>
+            <Input variant="filled" type="text" />
+          </FormControl>
+
+          <FormControl>
+            <FormControl.Label>Username</FormControl.Label>
+            <Input variant="filled" type="text" />
+          </FormControl>
+
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input variant="filled" type="password" />
+          </FormControl>
+        </VStack>
+
+        <VStack alignItems="center">
+          <Button width="full">Register</Button>
+
+          <Text mt="6" mb="2">
+            Already have an account?
+          </Text>
+          <Button
+            variant="outline"
+            onPress={() => navigation.navigate('Login')}>
+            Login
+          </Button>
+        </VStack>
+      </VStack>
     </View>
   );
 }
