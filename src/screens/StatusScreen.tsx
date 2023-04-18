d0e854b1d466cefc9d0bc49ca99f432a84a2d3e4
patrikfejda <patrikfejda@gmail.com>
2023-04-18 18:05:34 +0200

    acces to location

--- src/screens/StatusScreen.tsx
+++ src/screens/StatusScreen.tsx
@@ -1,24 +1,53 @@
-import { Text, View, Button } from 'native-base';
-import React from 'react';
-import type { TabScreenProps } from '../types/navigation';
-import Geolocation from '@react-native-community/geolocation';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'native-base';
+import Geolocation from 'react-native-geolocation-service';
+import { check, PERMISSIONS, request } from 'react-native-permissions';
 
-export default function StatusScreen({}: TabScreenProps<'Status'>) {
-  const getLocation = () => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        console.log('Latitude:', position.coords.latitude);
-        console.log('Longitude:', position.coords.longitude);
-      },
-      (error) => console.log(error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
+export default function StatusScreen() {
+  const [location, setLocation] = useState(null);
+
+  const getLocation = async () => {
+    try {
+      const permissionStatus = await check(PERMISSIONS.ANDROID.LOCATION_WHEN_IN_USE);
+      if (permissionStatus === 'granted') {
+        console.log("granted");
+        Geolocation.getCurrentPosition(
+          (position) => {
+            setLocation(position);
+          },
+          (error) => {
+            console.log(error);
+          },
+          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
+      } else {
+        console.log("not granted");
+        const permissionRequest = await request(PERMISSIONS.ANDROID.LOCATION_WHEN_IN_USE);
+        console.log("stop");
+        console.log(permissionRequest);
+        if (permissionRequest === 'granted') {
+          Geolocation.getCurrentPosition(
+            (position) => {
+              setLocation(position);
+            },
+            (error) => {
+              console.log(error);
+            },
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+          );
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <View>
-      <Text fontSize="md">Ahoj, ako sa mas?</Text>
-      <Button onPress={getLocation}>GET LOCATION</Button>
+      <Text>Location: {location ? `${location.coords.latitude}, ${location.coords.longitude}` : 'unknown'}</Text>
+      <Button onPress={getLocation}>
+        <Text>Get Location</Text>
+      </Button>
     </View>
   );
 }
