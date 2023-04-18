009011b3659119d695498c3c4ef624efe86b3baa
patrikfejda <patrikfejda@gmail.com>
2023-04-18 17:49:57 +0200

    mvp

--- src/screens/StatusScreen.tsx
+++ src/screens/StatusScreen.tsx
@@ -1,11 +1,24 @@
-import {Text, View} from 'native-base';
+import { Text, View, Button } from 'react-native';
 import React from 'react';
-import type {TabScreenProps} from '../types/navigation';
+import type { TabScreenProps } from '../types/navigation';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function StatusScreen({}: TabScreenProps<'Status'>) {
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log('Latitude:', position.coords.latitude);
+        console.log('Longitude:', position.coords.longitude);
+      },
+      (error) => console.log(error),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  };
+
   return (
     <View>
-      <Text>TODO STATUS</Text>
+      <Text>TODO 123</Text>
+      <Button title="Get Location" onPress={getLocation} />
     </View>
   );
 }
