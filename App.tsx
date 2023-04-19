1819599ec254bd5647aeb9ae82a306db7c73d055
Matej Pavlík <Snip3rSVK@gmail.com>
2023-04-19 11:19:29 +0200

    Edit Native Base input width to match Figma and use Immer for the navigationTheme

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
 import {DarkTheme, NavigationContainer} from '@react-navigation/native';
+import {produce} from 'immer';
 import {NativeBaseProvider, extendTheme} from 'native-base';
 import React from 'react';
 import {Provider as StoreProvider} from 'react-redux';
 import MainNavigator from './src/navigators/MainNavigator';
 import {store} from './src/store';
 
+const navigationTheme = produce(DarkTheme, draftTheme => {
+  draftTheme.colors.background = '#171717';
+});
+
 const nativeBaseConfig = {
   useSystemColorMode: false,
   initialColorMode: 'dark',
 };
-
-const nativeBaseTheme = extendTheme({config: nativeBaseConfig});
-const navigationTheme = {
-  ...DarkTheme,
-  colors: {
-    ...DarkTheme.colors,
-    background: '#171717',
+const nativeBaseTheme = extendTheme({
+  config: nativeBaseConfig,
+  components: {
+    Input: {
+      baseStyle: {
+        py: '5px',
+      },
+    },
   },
-};
+});
 
 function App(): JSX.Element {
   return (
