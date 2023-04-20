cfdbc87e41b3f260357b0bb9641e041fdf02db3a
patrikfejda <patrikfejda@gmail.com>
2023-04-20 17:37:11 +0200

    put down

--- src/components/AppPopup.tsx
+++ src/components/AppPopup.tsx
@@ -1,24 +1,47 @@
-import { getUserInitials } from '../utils/initials';
 import React, { useState } from 'react';
-import { Alert, Modal, Button, Text, Pressable, View } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { Alert, Modal, Pressable, Text, View } from 'react-native';
 
 export default function AppPopup({ view }) {
   const [modalVisible, setModalVisible] = useState(false);
   return (
     <View>
       <Modal
-        transparent={false}
+        animationType="slide"
+        transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
           Alert.alert('Modal has been closed.');
           setModalVisible(!modalVisible);
         }}>
-        <View>{view}</View>
-        <Pressable onPress={() => setModalVisible(!modalVisible)}>
-          <Text>HIDE</Text>
-        </Pressable>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+          }}>
+          {view}
+          <Pressable
+            style={{
+              backgroundColor: '#fff',
+              alignSelf: 'flex-end',
+              marginRight: 20,
+              marginBottom: 20,
+              padding: 10,
+              borderRadius: 20,
+            }}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <MaterialIcons name="close" size={10} color="#000" />
+          </Pressable>
+        </View>
       </Modal>
-      <Pressable onPress={() => setModalVisible(true)}>
+      <Pressable
+        style={{
+          backgroundColor: '#fff',
+          alignSelf: 'center',
+          padding: 10,
+          borderRadius: 20,
+        }}
+        onPress={() => setModalVisible(true)}>
         <Text>SHOW</Text>
       </Pressable>
     </View>
