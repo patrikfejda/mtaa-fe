76b738941b4079ff3d83bfbaed7c55f17f20fe11
patrikfejda <patrikfejda@gmail.com>
2023-04-20 14:22:55 +0200

    run on load

--- src/screens/StatusScreen.tsx
+++ src/screens/StatusScreen.tsx
@@ -33,6 +33,40 @@ export default function StatusScreen() {
 
   const sendStatus = async () => {
     console.log("sendStatus");
+    const permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    if (permissionStatus === RESULTS.GRANTED) {
+      console.log("permission granted");
+    }
+    else if (permissionStatus === RESULTS.DENIED) {
+      const permissionRequest = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      if (permissionRequest === RESULTS.GRANTED) {
+        await Geolocation.getCurrentPosition(
+          (position) => {
+            setForm({...form, longitude: position.coords.longitude, latitude: position.coords.latitude})
+          },
+          (error) => {
+            console.log(error);
+          },
+          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
+      } else {
+        Alert.alert(
+          'Permission denied',
+          'You need to grant access to location to use this feature.',
+          [
+            { text: 'Cancel', style: 'cancel' },
+            {
+              text: 'Open Settings',
+              onPress: () => {
+                openSettings().catch(() => console.warn('cannot open settings'));
+              },
+            },
+          ]
+          );
+      }
+    }
+
+
     await getLocation();
     console.log(form);
     createStatus(form);
@@ -55,33 +89,6 @@ export default function StatusScreen() {
           },
           { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
         );
-      } else if (permissionStatus === RESULTS.DENIED) {
-        const permissionRequest = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-        if (permissionRequest === RESULTS.GRANTED) {
-          await Geolocation.getCurrentPosition(
-            (position) => {
-              setForm({...form, longitude: position.coords.longitude, latitude: position.coords.latitude})
-            },
-            (error) => {
-              console.log(error);
-            },
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-          );
-        } else {
-          Alert.alert(
-            'Permission denied',
-            'You need to grant access to location to use this feature.',
-            [
-              { text: 'Cancel', style: 'cancel' },
-              {
-                text: 'Open Settings',
-                onPress: () => {
-                  openSettings().catch(() => console.warn('cannot open settings'));
-                },
-              },
-            ]
-            );
-        }
       }
     } catch (error) {
       console.log(error);
